63fb77f6527b40b18c1976d2123002f5c173efd6
Tiago Luis Souza Pantoja <tiagopantoja1998@gmail.com>
2023-07-27 16:24:01 -0300

    Adicionando arquivos principais do projeto

---
+++ SistemaDeTarefas/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeTarefas.Models
+{
+
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public String? Nome { get; set; }
+        public String? Email { get; set; }
+    }
+}
